628ec8d18f1711da7b2077f8668b0aaaa242e0b1
Zach Gikakis <zmgikakis@gmail.com>
2025-01-02 10:31:24 -0500

    Update and rename censustrade_create_df.py to censustrade_create_bulk_df.py
    added more detailed documentation

--- censustrade_create_bulk_df.py
+++ censustrade_create_bulk_df.py
@@ -1,9 +1,9 @@
-# Retrieves import and export data from U.S. Census International Trade Data API and converts to pandas dataframe
+# Retrieves import and export data from U.S. Census International Trade Data API for a specified date range and converts to pandas dataframe
 # Specify Variables: start_date, end_date, 10 digit HS codes, and fields to retreive 
 # imp_fields and exp_fields lists must be in the same order ie: match corresponding imp and exp fields
 # API documentation, including possible fields - https://www.census.gov/foreign-trade/reference/guides/Guide_to_International_Trade_Datasets.pdf
 # Request free API key from census.gov - or remove key parameter from params if making less than 500 api calls per day
-# Accepts 10 digit HS codes - https://www.flexport.com/data/
+# Accepts 10 digit HS codes - https://hts.usitc.gov/
 # can typically return data from current month -2 (can return Oct 2024 data in Dec 2024)
 
 
